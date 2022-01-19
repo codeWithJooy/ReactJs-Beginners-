c04719d16097891a220bbc4aa7547270dbb62426
codeWithJooy <70627163+codeWithJooy@users.noreply.github.com>
2022-01-19 13:07:02 +0530

    Create Header.js

---
+++ airbnbExperience(clone)/src/Components/Header.js
@@ -0,0 +1,11 @@
+import Vector from '../images/Vector.png'
+
+function Header(){
+    return(
+        <div className="head_section">
+            <img src={Vector} />
+        </div>
+    )
+}
+
+export default Header
