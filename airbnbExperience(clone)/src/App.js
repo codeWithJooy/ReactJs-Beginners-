72c54b01a7daee71126dec2db07091c6a9191a6b
codeWithJooy <70627163+codeWithJooy@users.noreply.github.com>
2022-01-20 09:32:21 +0530

    JavaScript Files Added

---
+++ airbnbExperience(clone)/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+import CardSection from './Components/CardSection'
+function App() {
+  return (
+     <div>
+       <Header />
+       <CardSection />
+     </div>
+  );
+}
+
+export default App;
